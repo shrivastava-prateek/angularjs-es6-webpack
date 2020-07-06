cb387e22b4644b418ef5c901bd8e7fd1fce621cb
shrivastava-prateek <prateek.shrivastava@noreply.github.com>
2020-07-07 00:18:12 +0530

    ..

--- karma.conf.js
+++ karma.conf.js
@@ -61,10 +61,11 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     //        browsers: ['Chrome', 'ChromeCanary', 'FirefoxAurora', 'Safari', 'PhantomJS'],
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: false,
+    concurrency: Infinity
   });
 };
